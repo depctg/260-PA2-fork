50f7605b248051ac0fadbf66d6600d61d287cf76
Ubuntu <ubuntu@ip-172-31-26-232.us-west-2.compute.internal>
2020-10-28 05:17:33 +0000

    verify device results using openblas

--- mmpy_host.cpp
+++ mmpy_host.cpp
@@ -1,26 +1,26 @@
 #include "types.h"
+#include "cblas.h"
 void
-matMulHost(_DOUBLE_  *C, const _DOUBLE_  *A, const _DOUBLE_  *B, unsigned int m, unsigned int n)
+matMulHost(_DOUBLE_  *C, const _DOUBLE_  *A, const _DOUBLE_  *B, unsigned int M, unsigned int N)
 {
-    for (unsigned int i = 0; i < m; i++)
-        for (unsigned int j = 0; j < n; j++) {
-            _DOUBLE_ sum = 0;
-            for (unsigned int k = 0; k < n; k++) 
-                sum += A[i * n + k] * B[k * n + j];
-            C[i * n + j] = (_DOUBLE_) sum;
-        }
+    const _DOUBLE_ Beta  = 0.0;
+    const _DOUBLE_ Alpha = 1.0;
+    const int K = N;
+    const int LDA = N, LDB = N, LDC = N;
+    const enum CBLAS_TRANSPOSE transA = CblasNoTrans;
+    const enum CBLAS_TRANSPOSE transB = CblasNoTrans;
+    cblas_dgemm( CblasRowMajor, transA, transB, M, N, K,
+                 Alpha, A, LDA, B, LDB, Beta, C, LDC );
 }
 
 void
-reference_dgemm(unsigned int n, _DOUBLE_ Alpha, _DOUBLE_  *A, _DOUBLE_  *B, _DOUBLE_  *C)
+reference_dgemm(unsigned int N, _DOUBLE_ Alpha, _DOUBLE_  *A, _DOUBLE_  *B, _DOUBLE_  *C)
 {
-    const _DOUBLE_ Beta = 1.0;
-    for (unsigned int i = 0; i < n; i++)
-        for (unsigned int j = 0; j < n; j++) {
-            _DOUBLE_ sum = 0;
-            for (unsigned int k = 0; k < n; k++) 
-                sum += A[i * n + k] * B[k * n + j];
-            _DOUBLE_ *cp = &C[i * n + j];
-            *cp =  Alpha * sum +  (Beta * *cp);
-        }
+    const _DOUBLE_ Beta  = 1.0;
+    const int M = N, K = N;
+    const int LDA = N, LDB = N, LDC = N;
+    const enum CBLAS_TRANSPOSE transA = CblasNoTrans;
+    const enum CBLAS_TRANSPOSE transB = CblasNoTrans;
+    cblas_dgemm( CblasRowMajor, transA, transB, M, N, K,
+                 Alpha, A, LDA, B, LDB, Beta, C, LDC );
 }
